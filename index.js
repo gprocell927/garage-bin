e0d4bea19938b2e5bf09ff0af45042791a6f0d19
Gabi Procell <gprocell927@gmail.com>
2017-04-13 12:33:23 -0600

    adds getItems function

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const garageDoor = document.querySelector('.garage-door')
 
 openGarageButton.addEventListener('click', (e) => {
   toggleGarageDoorDisplay(e)
+  getItems()
   renderNewItemForm(e)
 })
+
+function getItems() {
+  const server = ('/api/items')
+  fetch(server, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+  })
+  .then(response => response.json())
+  .then(response => document.querySelector('.item-shelf').innerHTML = response.reduce((acc, item) => `${acc} <ul data-id${item.id} class="item-list">${item.name}</ul>`, '')
+  )
+}
 
 function toggleGarageDoorDisplay(e){
   e.preventDefault()

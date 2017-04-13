537d5b3e868bc26aee6ddfd8333246e7e7fd733d
Gabi Procell <gprocell927@gmail.com>
2017-04-13 12:45:06 -0600

    Adds knexfile and creates items db

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Garage Bin'
-app.locals.items = {
-  id: 1,
-  name: 'rubber chicken'
-}
+app.locals.items = {}
 
 app.get('/', (request, response) => {
   response.send('Welcome to the garage bin!')
@@ -18,7 +15,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/items', (request, response) => {
   const items = app.locals.items
-  response.json({ items })
+  response.status(200).json({ items })
 })
 
 app.get('/api/items/:id', (request, response) => {

50aa01f0e5cbe981d554a206ec25c7c8cc05c842
Gabi Procell <gprocell927@gmail.com>
2017-04-13 10:27:30 -0600

    Adds module conditional to server listener. Exports app module and installs mocha module

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (request, response) => {
   response.send('Welcome to the garage bin!')
 })
 
+if(!module.parent){
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
-})
+  })
+}
+
+module.exports = app

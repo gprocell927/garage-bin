b18c5b26c0430e5caddb5b39247e19bbd243d7d5
Gabi Procell <gprocell927@gmail.com>
2017-04-13 16:56:31 -0600

    Adds input fields for item reason and cleanliness drop down menu

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 const garageDoor = document.querySelector('.garage-door')
 const openGarageButton = document.querySelector('.open-garage-btn')
+const newItemForm = document.querySelector('.item-form')
 
 openGarageButton.addEventListener('click', () => {
   toggleGarageDoorDisplay()
@@ -11,6 +12,7 @@ function toggleGarageDoorDisplay(){
   } else {
     garageDoor.style.display = 'none'
     showItems()
+    renderNewItemForm()
   }
 }
 
@@ -25,4 +27,22 @@ function showItems(){
   })
   .then(res => res.json())
   .then(res => document.querySelector('.item-shelf').innerHTML = res.reduce((acc, item) => `${acc} <ul data-id=${item.id} class="item-list">${item.name}</ul>`, ''))
+}
+
+function renderNewItemForm () {
+  newItemForm.innerHTML =
+  `<section>
+    <aside class="new-item-container">
+      <span>Name of junk item: </span>
+      <input placeholder="New item name" />
+      <span>Why do you hang on to this crap?</span>
+      <input placeholder="Lame reason here" />
+      <span>Select the cleanliness of this item:</span>
+      <select>
+        <option value='Sparkling'>Sparkling</option>
+        <option value='Dusty'>Dusty</option>
+        <option value='Rancid'>Rancid</option>
+      </select>
+    </aside>
+  </section>`
 }

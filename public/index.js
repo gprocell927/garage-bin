53154cab7bad8530ab868de34fef8b72b805e680
Gabi Procell <gprocell927@gmail.com>
2017-04-13 17:44:08 -0600

    All qualities of items display to shelf. Having async issues, though

--- public/index.js
+++ public/index.js
@@ -1,10 +1,40 @@
 const garageDoor = document.querySelector('.garage-door')
 const openGarageButton = document.querySelector('.open-garage-btn')
 const newItemForm = document.querySelector('.item-form')
+const submitButton = document.querySelector('.submit-item-btn')
 
 openGarageButton.addEventListener('click', () => {
   toggleGarageDoorDisplay()
 })
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  postNewItem()
+})
+
+function postNewItem(){
+  const userInput = document.querySelector('.item-name-input')
+  const userReason = document.querySelector('.item-reason-input')
+  const cleanliness = document.querySelector('.cleanliness-selection')
+  const server = ('/api/items')
+
+  fetch(server, {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name:userInput.value,
+      reason:userReason.value,
+      cleanliness: cleanliness.value
+    })
+  })
+  .then(res => res.json())
+  .then(res => showItems())
+  userInput.value = ''
+  }
+
 
 function toggleGarageDoorDisplay(){
   if(garageDoor.style.display == 'none'){
@@ -12,7 +42,6 @@ function toggleGarageDoorDisplay(){
   } else {
     garageDoor.style.display = 'none'
     showItems()
-    renderNewItemForm()
   }
 }
 
@@ -26,23 +55,5 @@ function showItems(){
     },
   })
   .then(res => res.json())
-  .then(res => document.querySelector('.item-shelf').innerHTML = res.reduce((acc, item) => `${acc} <ul data-id=${item.id} class="item-list">${item.name}</ul>`, ''))
-}
-
-function renderNewItemForm () {
-  newItemForm.innerHTML =
-  `<section>
-    <aside class="new-item-container">
-      <span>Name of junk item: </span>
-      <input placeholder="New item name" />
-      <span>Why do you hang on to this crap?</span>
-      <input placeholder="Lame reason here" />
-      <span>Select the cleanliness of this item:</span>
-      <select>
-        <option value='Sparkling'>Sparkling</option>
-        <option value='Dusty'>Dusty</option>
-        <option value='Rancid'>Rancid</option>
-      </select>
-    </aside>
-  </section>`
+  .then(res => document.querySelector('.item-shelf').innerHTML = res.reduce((acc, item) => `${acc} <ul data-id=${item.id} class="item-list">Name: ${item.name}, Reason: ${item.reason}, Cleanliness: ${item.cleanliness}</ul>`, ''))
 }

82f2d87dbacae7bba9a8482cb042b41754129b42
Gabi Procell <gprocell927@gmail.com>
2017-04-13 10:54:14 -0600

    Creates get endpoint for all items

---
+++ test/server-test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const app = require('../server')
+
+describe('Server', () => {
+
+  it('should exist', () => {
+    assert(app)
+  })
+
+})//end of describe server block

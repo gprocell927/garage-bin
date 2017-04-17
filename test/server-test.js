aab677287612d1d7c1a207927312081761d0828b
Gabi Procell <gprocell927@gmail.com>
2017-04-17 09:51:31 -0600

    Adds test seed file and post endpoint test

--- test/server-test.js
+++ test/server-test.js
@@ -1,9 +1,22 @@
 const chai = require('chai')
 const expect = chai.expect
-const chaiHTtp = require('chai-http')
+const chaiHttp = require('chai-http')
 const app = require('../server.js')
 
+const environment = 'test';
+const configuration = require('../knexfile')[environment];
+const knex = require('knex')(configuration);
+chai.use(chaiHttp)
+
 describe('Server', () => {
+  context('app', () => {
+  it('should exist', () => {
+    expect(app).to.exist
+    })
+  })
+})
+
+describe('Endpoints', () => {
   beforeEach((done) => {
     knex.migrate.rollback()
     .then(() => {
@@ -24,11 +37,7 @@ describe('Server', () => {
     })
   })
 
-  context('app', () => {
-  it('should exist', () => {
-    assert(app)
-  })
-}) //end of context app block
+   //end of context app block
 
   context('GET /api/items', () => {
     it('should return all items', (done) => {
@@ -36,10 +45,43 @@ describe('Server', () => {
       .get('/api/items')
       .end((err, res) => {
         expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.be.a('array')
+        expect(res.body.length).to.equal(2)
+        expect(res.body[0]).to.have.property('name')
+        expect(res.body[0].name).to.equal('Rubber Chicken')
+        expect(res.body[0]).to.have.property('reason')
+        expect(res.body[0].reason).to.equal('It is fun')
+        expect(res.body[0]).to.have.property('cleanliness')
+        expect(res.body[0].cleanliness).to.equal('Dusty')
         done()
       })
     })
-  })
+  }) // end of GET /api/items block
+
+  context('POST /api/items', () => {
+      it('should add an item', (done) => {
+        chai.request(app)
+        .post('/api/items')
+        .send({
+          name: 'Pink Marshmallow Peeps from 1995',
+          reason: 'They never go bad and they\'re delicious',
+          cleanliness:'Rancid'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200)
+          expect(res).to.be.json
+          expect(res.body).to.be.a('array')
+          expect(res.body[0]).to.have.property('name')
+          expect(res.body[0].name).to.equal('Pink Marshmallow Peeps from 1995')
+          expect(res.body[0]).to.have.property('reason')
+          expect(res.body[0].reason).to.equal('They never go bad and they\'re delicious')
+          expect(res.body[0]).to.have.property('clcleanliness')
+          expect(res.body[0].reason).to.equal('Rancid')
+          done()
+        })
+      })
+    }) //end of POST /api/items
 
 
 })//end of describe server block

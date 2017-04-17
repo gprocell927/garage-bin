be4671160a683e2789b8b6d24dfccbfc25cbb062
Gabi Procell <gprocell927@gmail.com>
2017-04-17 09:32:52 -0600

    Adds before and after hooks to test file. Fixes test directory in package.json

--- test/server-test.js
+++ test/server-test.js
@@ -1,10 +1,45 @@
-const assert = require('assert')
-const app = require('../server')
+const chai = require('chai')
+const expect = chai.expect
+const chaiHTtp = require('chai-http')
+const app = require('../server.js')
 
 describe('Server', () => {
+  beforeEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+        .then(() => {
+          return knex.seed.run()
+          .then(() => {
+            done()
+          })
+        })
+    })
+  })
 
+  afterEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      done()
+    })
+  })
+
+  context('app', () => {
   it('should exist', () => {
     assert(app)
   })
+}) //end of context app block
+
+  context('GET /api/items', () => {
+    it('should return all items', (done) => {
+      chai.request(app)
+      .get('/api/items')
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        done()
+      })
+    })
+  })
+
 
 })//end of describe server block
